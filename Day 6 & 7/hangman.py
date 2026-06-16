eb333f211af2eb6a7da523703a00354155f84d06
heisenberglead-max <heisenberglead@gmail.com>
2026-06-16 19:06:57 +0545

    Functions and While loop

---
+++ Day 6 & 7/hangman.py
@@ -0,0 +1,127 @@
+import random
+# word_list = [
+#     "table", "chair", "window", "door", "computer", "phone", "book", "pen", "pencil", "paper",
+#     "bottle", "cup", "plate", "spoon", "fork", "knife", "clock", "watch", "wallet", "bag",
+#     "shoe", "sock", "shirt", "pants", "jacket", "hat", "key", "lock", "mirror", "comb",
+#     "brush", "soap", "towel", "bed", "pillow", "blanket", "lamp", "desk", "shelf", "couch",
+#     "television", "remote", "camera", "radio", "guitar", "piano", "drum", "ball", "bat", "bicycle",
+#     "car", "truck", "bus", "train", "airplane", "boat", "ship", "wheel", "engine", "tire",
+#     "hammer", "screwdriver", "wrench", "pliers", "saw", "nail", "screw", "drill", "ladder", "rope",
+#     "basket", "box", "bucket", "shovel", "broom", "mop", "sponge", "scissors", "tape", "glue",
+#     "candle", "match", "lighter", "pipe", "wire", "cable", "plug", "switch", "battery", "bulb",
+#     "coin", "bill", "card", "ring", "necklace", "bracelet", "earring", "glasses", "umbrella", "purse"
+# ]
+word_list = ['apple','car','door']
+hangman = ['''
+              +---+
+              |   |
+                  |
+                  |
+                  |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+                  |
+                  |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+              |   |
+                  |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+             /|   |
+                  |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+             /|\  |
+                  |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+             /|\  |
+             /    |
+                  |
+            =========''', '''
+              +---+
+              |   |
+              O   |
+             /|\  |
+             / \  |
+                  |
+            =========''']
+
+print("WELCOME TO THE HANGMAN GAME!!!!!!!!!")
+print('''  +---+
+           |   |
+           O   |
+          /|\  |
+          / \  |
+               |
+         ========= ''')
+game=''
+display = []
+hold_letter = []
+game_over = False
+while not game_over:
+    word = random.choice(word_list)
+    word_len = len(word)
+    no_of_guess = 6
+    hangman_face = 0
+    display = ["_"] * word_len
+    hold_letter = []
+
+    print("Your word is : ")
+    print(" ".join(display))
+    while no_of_guess != 0 and hangman_face < 6:
+        print(hangman[hangman_face])
+        guess_letter = input("Please Guess a letter: ").lower()
+        guess_value = 0
+        if guess_letter in hold_letter:
+            print("You already guessed that letter.")
+        else:
+            hold_letter.append(guess_letter)
+            for i, x in enumerate(word):
+                if guess_letter == x:
+                    display[i] = x
+                    guess_value += 1
+            if guess_value == 0:
+                hangman_face += 1
+                no_of_guess -= 1
+                print(hangman[hangman_face])
+            else:
+                print(hangman[hangman_face])
+        print(f"Your Word to guess is: {' '.join(display)}")
+        print(f"You've {no_of_guess} left ")
+        if no_of_guess == 0:
+            print(f"Your word was {word}")
+            print("GAMEEEE OVEERRRR!!!")
+            game_over = True
+            break
+        if '_' not in display:
+            print("You win")
+            game_over = True
+            break
+        
+
+
+    
+        
+        
+                
+                
+                   
+        
+    
--- README.md
+++ README.md
@@ -3,10 +3,12 @@ This is my daily set challenge of Building with python for 100 days with cool pr
 
 Day-1: Today about learning syntax and creating a simple name generator. (Learning about Variables)
 
-Day-2: Today is about building a tip calculator. (Learning about operators)
+Day-2: Today about building a tip calculator. (Learning about operators)
 
-Day-3: Today is  about conditional statements.(if/else) and  build a simple treasure hunting task. 
+Day-3: Today  about conditional statements.(if/else) and  build a simple treasure hunting task. 
 
-Day-4: Today is about List and Modules(especially Random). Added a simple Rock, Paper, Scissors Game.
+Day-4: Today  about List and Modules(especially Random). Added a simple Rock, Paper, Scissors Game.
 
-Day-5: Today is about Loops in Python."Simple Password Generator"
+Day-5: Today about Loops in Python."Simple Password Generator"
+
+Day 6 & 7: Today about functions and Day 6 in other application and Day 7- Hangman game is here.
